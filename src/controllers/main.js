0a0d305e523f871e4d6ce518d988a65dddbf52ac
Spaghetti <alejandrolpz0619@gmail.com>
2020-12-05 15:01:34 -0400

    main release

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -4,7 +4,17 @@ const baseUrl = `https://rickandmortyapi.com/api`
 
 const controllers = {
     home: async (req, res) => {
-        res.render('welcome')
+        try {
+            let characterHome = await axios.get(baseUrl + '/character');
+            let characterArray = characterHome.data.results;
+            for (let i = 0; i <= 13 ; i++){
+                characterArray.pop();
+            }
+            res.render('welcome', { characterArray });
+            
+        } catch (e) {
+            console.error(e)
+       }
     },
 
     searchCharacter: async (req, res) => {
@@ -12,7 +22,6 @@ const controllers = {
             const { searchName } = req.body;
             let character = await axios.get(baseUrl + '/character/' + `?name=${searchName}`);
             let info = character.data.results;
-            console.log(info);
             res.render('character', { info });
 
         } catch (e) {
